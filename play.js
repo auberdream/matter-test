2a87e9b3cab755316e7450e8833f18b38fa3b9a5
Ellie <elliewem@gmail.com>
2018-03-04 22:38:55 +0000

    Add irregular

--- play.js
+++ play.js
@@ -4,6 +4,7 @@ var Engine = Matter.Engine,
     World = Matter.World,
     Mouse = Matter.Mouse,
     MouseConstraint = Matter.MouseConstraint,
+    Vector = Matter.Vector,
     Bodies = Matter.Bodies;
 
 // create an engine
@@ -20,11 +21,12 @@ var render = Render.create({
 var circle = Bodies.circle(400, 200, 80);
 var box = Bodies.rectangle(450, 50, 80, 80);
 var polygon = Bodies.polygon(300, 200, 9, 50);
-var trapezoid = Bodies.trapezoid(200, 300, 20, 300, 4)
+var trapezoid = Bodies.trapezoid(200, 300, 20, 300, 4);
+var irregular = Bodies.fromVertices(200, 200, [Vector.create(20, 30), Vector.create(30, -10), Vector.create(0, -70), Vector.create(-50, 0), Vector.create(0, 50)])
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
-World.add(engine.world, [circle, box, polygon, trapezoid, ground]);
+World.add(engine.world, [circle, box, polygon, trapezoid, irregular, ground]);
 
 // add mouse control
 var mouse = Mouse.create(render.canvas),
@@ -33,7 +35,7 @@ var mouse = Mouse.create(render.canvas),
         constraint: {
             stiffness: 0.2,
             render: {
-                visible: false
+                visible: true
             }
         }
     });
