008a05effdb659bedeb4cccd82f18f4188f4bf75
Ellie <elliewem@gmail.com>
2018-03-04 23:27:53 +0000

    Add constraint

--- play.js
+++ play.js
@@ -5,7 +5,8 @@ var Engine = Matter.Engine,
     Mouse = Matter.Mouse,
     MouseConstraint = Matter.MouseConstraint,
     Vector = Matter.Vector,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Constraint = Matter.Constraint;
 
 // create an engine
 var engine = Engine.create()
@@ -18,15 +19,27 @@ var render = Render.create({
 });
 
 // create objects and a ground
-var circle = Bodies.circle(400, 200, 80);
+var circle = Bodies.circle(400, 200, 40);
 var box = Bodies.rectangle(450, 50, 80, 80);
 var polygon = Bodies.polygon(300, 200, 9, 50);
-var trapezoid = Bodies.trapezoid(200, 300, 20, 300, 4);
+var trapezoid = Bodies.trapezoid(0, 300, 200, 300, 15, { isStatic: true });
 var irregular = Bodies.fromVertices(200, 200, [Vector.create(20, 30), Vector.create(30, -10), Vector.create(0, -70), Vector.create(-50, 0), Vector.create(0, 50)])
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var ground = Bodies.rectangle(400, 610, 810, 20, { isStatic: true });
+var wall_left = Bodies.rectangle(-10, 300, 20, 800, { isStatic: true });
+var wall_right = Bodies.rectangle(810, 300, 20, 800, { isStatic: true });
+
+// constraints
+var constraint1 = Constraint.create({
+  bodyA: circle,
+  pointA: { x: 50, y: 50 },
+  bodyB: box,
+  // pointB: { x: 90, y: 90 },
+  damping: 0.04,
+  stiffness: 0.007,
+});
 
 // add all of the bodies to the world
-World.add(engine.world, [circle, box, polygon, trapezoid, irregular, ground]);
+World.add(engine.world, [circle, box, polygon, trapezoid, irregular, ground, wall_left, wall_right, constraint1]);
 
 // add mouse control
 var mouse = Mouse.create(render.canvas),
